837e100f2ea67a7d0919c4516dc970a012446ea2
janWyler1 <jan.wyler@stud.bfstg.ch>
2026-04-02 15:15:43 +0200

    Add Terraform configuration for S3 website setup

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "janwyler1-einkaufsliste"
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket_pab" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  depends_on = [aws_s3_bucket_public_access_block.website_bucket_pab]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "index.html"
+  source       = "frontend/index.html"
+  content_type = "text/html"
+  etag         = filemd5("frontend/index.html")
+}
+
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.website_config.website_endpoint
+}
